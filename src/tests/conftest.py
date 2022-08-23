0b96ea60c93301383eb645a5188e2c6a205a8e07
MarlonHenrique7 <marlonhenrique777@gmail.com>
2022-08-22 22:11:25 -0300

    test setup

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,12 +1,11 @@
-# src/tests/conftest.py
-
 import pytest
+from src.api.models import User
 
-from src import app, db
-
+from src import create_app, db  
 
 @pytest.fixture(scope='module')
 def test_app():
+    app = create_app()
     app.config.from_object('src.config.TestingConfig')
     with app.app_context():
         yield app  # testing happens here
@@ -18,3 +17,12 @@ def test_database():
     yield db  # testing happens here
     db.session.remove()
     db.drop_all()
+
+@pytest.fixture(scope='function')
+def add_user():
+    def _add_user(username, email):
+        user = User(username=username, email=email)
+        db.session.add(user)
+        db.session.commit()
+        return user
+    return _add_user
